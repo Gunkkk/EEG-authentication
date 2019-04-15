b715cba280ecbfc2cacbf74610384429d239ee1a
Gunkkk <840745950@qq.com>
2019-04-15 21:19:51 +0800

    data process

--- mat.py
+++ mat.py
@@ -10,8 +10,11 @@ a = mat['DREAMER']
 def person(p_i):
     return a['Data'][0,0][0,p_i]
 
-def person_clip(p_i,c_i):
+def baseline_person_clip(p_i,c_i):
     return person(p_i)['EEG'][0,0]['baseline'][0,0][c_i,:][0]
+
+def stimuli_person_clip(p_i,c_i):
+        return person(p_i)['EEG'][0,0]['stimuli'][0,0][c_i,:][0]
 
 #print(person_clip(1,1))
 print('========')
@@ -29,23 +32,57 @@ print('========')
 #     plt.plot(np.arange(0,14),person_clip(0,clip_i)[0,:])
 
 # plt.show()
-baseline_clip = list()
-for clip_i in range(18):
-    label = None
-    data = None
-    baseline_clip0 = dict()
-    for  p_i in range(23):
-        print('personddddd',p_i)
-        if data is None:
-                data = person_clip(p_i,clip_i)[np.newaxis,:,:]
-        else:
-                data = np.append(data,person_clip(p_i,clip_i)[np.newaxis,:,:],axis=0) #samples*channels
-        if label is None:
-                label = np.array([p_i])
-        else:
-                label = np.append(label,[p_i],axis=0)
-    baseline_clip0['data'] = data
-    baseline_clip0['label'] = label
-    baseline_clip.append(baseline_clip0)
-np.array(baseline_clip)
-np.save('eeg_all.npy',baseline_clip)
+
+
+
+def baseline():
+    baseline_clip = list()
+    for clip_i in range(18):
+        label = None
+        data = None
+        baseline_clip0 = dict()
+        for  p_i in range(23):
+                print('personddddd',p_i)
+                if data is None:
+                        data = baseline_person_clip(p_i,clip_i)[np.newaxis,:,:]
+                else:
+                        data = np.append(data,baseline_person_clip(p_i,clip_i)[np.newaxis,:,:],axis=0) #samples*channels
+                if label is None:
+                        label = np.array([p_i])
+                else:
+                        label = np.append(label,[p_i],axis=0)
+        baseline_clip0['data'] = data
+        baseline_clip0['label'] = label
+        baseline_clip.append(baseline_clip0)
+    np.array(baseline_clip)
+    #print(baseline_clip.shape)
+    np.save('eeg_baseline.npy',baseline_clip)
+
+
+def stimuli():
+    stimuli_clip = list()
+    for clip_i in range(18):
+        label = None
+        data = None
+        stimuli_clip0 = dict()
+        for  p_i in range(23):
+               # print('personddddd',p_i)
+                if data is None:
+                        data = stimuli_person_clip(p_i,clip_i)[np.newaxis,:,:]
+                else:
+                        data = np.append(data,stimuli_person_clip(p_i,clip_i)[np.newaxis,:,:],axis=0) #samples*channels
+                if label is None:
+                        label = np.array([p_i])
+                else:
+                        label = np.append(label,[p_i],axis=0)
+        stimuli_clip0['data'] = data
+        stimuli_clip0['label'] = label
+        stimuli_clip.append(stimuli_clip0)
+        print(clip_i)
+    np.array(stimuli_clip)
+    np.save('eeg_stimuli.npy',stimuli_clip) # 23*25472*14
+
+if __name__ == "__main__":
+        print(stimuli_person_clip(0,0).shape) # 25472*14
+        print(baseline_person_clip(0,0).shape)
+        #stimuli()
--- num.py
+++ num.py
@@ -1,67 +1,115 @@
 import numpy as np
-a = np.load('eeg_all.npy')
-
-print(a.shape)
-print(a[17]['data'].shape)
-print(a[17]['label'])
-# a = np.arange(10)
-#print(a[10]['data']) 
-print('=========')
-b = dict()
-data = a[0]['data']
-label = a[0]['label']
-for i in range(1,6): #12345
-    data = np.append(data,a[i]['data'],axis=0)
-    label = np.append(label,a[i]['label'],axis=0)
-b['data'] = data
-b['label'] = label
-
-c=b['data'].shape
-print(c,b['label'].shape)
-eeg=dict()
-eeg['label'] = b['label']
-eeg['data'] = np.zeros(shape=(138,7680,6,6))
-eeg['data'][:,:,0,1] = b['data'][:,128:,0]
-eeg['data'][:,:,1,0] = b['data'][:,128:,1]
-eeg['data'][:,:,1,2] = b['data'][:,128:,2]
-eeg['data'][:,:,2,1] = b['data'][:,128:,3]
-eeg['data'][:,:,3,0] = b['data'][:,128:,4]
-eeg['data'][:,:,4,1] = b['data'][:,128:,5]
-eeg['data'][:,:,5,2] = b['data'][:,128:,6]
-eeg['data'][:,:,5,3] = b['data'][:,128:,7]
-eeg['data'][:,:,4,4] = b['data'][:,128:,8]
-eeg['data'][:,:,3,5] = b['data'][:,128:,9]
-eeg['data'][:,:,2,4] = b['data'][:,128:,10]
-eeg['data'][:,:,1,3] = b['data'][:,128:,11]
-eeg['data'][:,:,1,5] = b['data'][:,128:,12]
-eeg['data'][:,:,1,4] = b['data'][:,128:,13]
-
-print(eeg['data'][0,0,1,5])
-print('=====')
-print(b['data'][0,128,12])
-##验证
-eeg['data'].resize(6*23*6,10,128,6,6)
-print(eeg['data'][0,0,0,1,5],eeg['data'].shape)
-#eeg['data'] = eeg['data'].transpose(0,1,3,4,2)
-#print(eeg['data'][0,0,1,5,0],eeg['data'].shape)
 
 
 
-#完成处理 data格式为 （clips=6*persons=23*subsamples=6）*10s*6*6*128  =-》828*10*6*6*128
-eeg_label = np.zeros(shape=(6*23,6))
-print(eeg_label.shape)
-print(eeg['label'].size)
-for i in range(eeg['label'].size):
-    eeg_label[i][:] = eeg['label'][i]
 
-eeg_label.resize(6*23*6,1)
-print(eeg_label.shape)
-#print(eeg_label)
-eeg['label'] = eeg_label
-#       labelge格式为 clips*persons*subsamples*1=-》828*1
-np.array(eeg)
-np.save('eeg_data.npy',eeg)
+
+
+    
+def eegtodata(a,CLIP_NUM,trainname,testname,shuffle=False):
+    
+    b = dict()
+    data = a[0]['data']
+    label = a[0]['label']
+    for i in range(1,CLIP_NUM): #12345
+        data = np.append(data,a[i]['data'],axis=0)
+        label = np.append(label,a[i]['label'],axis=0)
+    b['data'] = data
+    b['label'] = label
+
+    c=b['data'].shape
+    print(c,b['label'].shape)
+    eeg=dict()
+    eeg['label'] = b['label']
+    eeg['data'] = np.zeros(shape=(23*CLIP_NUM,7680,6,6))
+    eeg['data'][:,:,0,1] = b['data'][:,128:,0]
+    eeg['data'][:,:,1,0] = b['data'][:,128:,1]
+    eeg['data'][:,:,1,2] = b['data'][:,128:,2]
+    eeg['data'][:,:,2,1] = b['data'][:,128:,3]
+    eeg['data'][:,:,3,0] = b['data'][:,128:,4]
+    eeg['data'][:,:,4,1] = b['data'][:,128:,5]
+    eeg['data'][:,:,5,2] = b['data'][:,128:,6]
+    eeg['data'][:,:,5,3] = b['data'][:,128:,7]
+    eeg['data'][:,:,4,4] = b['data'][:,128:,8]
+    eeg['data'][:,:,3,5] = b['data'][:,128:,9]
+    eeg['data'][:,:,2,4] = b['data'][:,128:,10]
+    eeg['data'][:,:,1,3] = b['data'][:,128:,11]
+    eeg['data'][:,:,1,5] = b['data'][:,128:,12]
+    eeg['data'][:,:,1,4] = b['data'][:,128:,13]
+
+    print(eeg['data'][0,0,1,5])
+    print('=====')
+    print(b['data'][0,128,12])
+    ##验证
+    eeg['data'].resize(CLIP_NUM*23*6,10,128,6,6)
+    print(eeg['data'][0,0,0,1,5],eeg['data'].shape)
+    #eeg['data'] = eeg['data'].transpose(0,1,3,4,2)
+    #print(eeg['data'][0,0,1,5,0],eeg['data'].shape)
+
+
+
+    #完成处理 data格式为 （clips=6*persons=23*subsamples=6）*10s*128*6*6  =-》828*10*128*6*6
+    eeg_label = np.zeros(shape=(CLIP_NUM*23,6))
+    print(eeg_label.shape)
+    print(eeg['label'].size)
+    for i in range(eeg['label'].size):
+        eeg_label[i][:] = eeg['label'][i]
+
+    eeg_label.resize(CLIP_NUM*23*6,1)
+    print(eeg_label.shape)
+    #print(eeg_label)
+    eeg['label'] = eeg_label
+    #       labelge格式为 clips*persons*subsamples*1=-》828*1
+   # print(type(eeg['data']))
+    #np.array(eeg)
+    if shuffle is True:
+        permutation = np.random.permutation(eeg['label'].size)
+        shufflez_data = eeg['data'][permutation,:,:,:,:]
+        shufflez_label = eeg['label'][permutation,:]
+
+    eeg['data'].resize(CLIP_NUM*23,6,10,128,6,6)
+    eeg['label'].resize(CLIP_NUM*23,6,1)
+    train_data = eeg['data'][:,:5,:,:,:,:].copy()
+    test_data = eeg['data'][:,5:,:,:,:,:].copy()
+    train_label = eeg['label'][:,:5,:].copy()
+    test_label = eeg['label'][:,5:,:].copy() #otherwise resize error 
+
+    print(train_data.shape,train_label.shape,test_data.shape,test_label.shape)
+    #print(type(train_data))
+    train_data.resize(CLIP_NUM*23*5,10,128,6,6)
+    test_data.resize(CLIP_NUM*23*1,10,128,6,6)
+    train_label.resize(CLIP_NUM*23*5,1)
+    test_label.resize(CLIP_NUM*23*1,1)
+    
+    # train_data = eeg['data'][:15*23*6]
+    # train_label = eeg['label'][:15*23*6]
+    # test_data = eeg['data'][15*23*6:]
+    # test_label = eeg['label'][15*23*6:]
+    print(train_data.shape,train_label.shape,test_data.shape,test_label.shape)
+    
+    train = dict()
+    test = dict()
+    train['data'] = train_data
+    train['label'] = train_label
+    test['data'] = test_data
+    test['label'] = test_label
+    np.array(train)
+    np.array(test)
+    np.save(trainname,train)
+    np.save(testname,test)
+    #print(a.shape)
+    #print(a[17]['data'].shape)
+    #print(a[17]['label'])
+    # a = np.arange(10)
+    #print(a[10]['data']) 
+    #print('=========')
 
 # b = a[np.newaxis,:]
 # b[1,2] = 2
 # print(b)
+if __name__ == "__main__":
+    a = np.load('eeg_all.npy')
+    #eegtodata(a,18,'eeg_baseline_train.npy','eeg_baseline_test.npy')
+    eegtodata(a,18,'eeg_baseline_train_shuffle.npy','eeg_baseline_test_shuffle.npy',shuffle=True)
+    #train (2070,10,128,6,6) (2070,1)
+    #test  (414,10,128,6,6)  (414,1)
